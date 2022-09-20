653766e4abb1e795a8fc1da84d4453da77b71c45
Boobeh123 <boobeh321@gmail.com>
2022-09-19 16:53:10 -1000

    Handling put & delete requests

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 const PORT = process.env.PORT || 3000;
-let DB_STRING = process.env.DB_STRING;
+let db,
+    DB_STRING = process.env.DB_STRING,
+    DB_NAME = 'motivational-quotes',
+    COLL_NAME = 'quotes';
 
 try {
     const mongoAtlasLogin = require('./config.js');
@@ -12,67 +15,66 @@ try {
     console.error(error);
 }
 
-
-// Connecting to MongoDB Atleast using Promises
 MongoClient.connect(DB_STRING)
 .then(client => {
-    console.log('Connected to Database');
-    const db = client.db('motivational-quotes');
-    const quotesCollection = db.collection('quotes');
-    app.set('view engine', 'ejs');  // Setting view engine to use EJS
-    app.use(bodyParser.urlencoded({ extended: true}))   // Place body-parser before CRUD handlers
-    app.use(bodyParser.json())  // Accepting PUT requests
-    app.use(express.static('public'));  // Tell Express to make this public folder accessible
-    app.get('/', (request, response) => {   // Using the sendFile method to serve an html page in the root directory
-        // response.sendFile(__dirname + '/index.html')
-        db.collection('quotes').find().toArray()
-        .then(results => {
-            console.log(results);
-            response.render('index.ejs', { quotes: results });
-        })
-        .catch(error => console.error(error));
-    });
-
-    // Using the insertOne method to add into a MongoDB collection
-    app.post('/quotes', (request, response) => {
-        quotesCollection.insertOne(request.body)
-        .then(result => {
-            console.log(result);
-            response.redirect('/');
-        })
-        .catch(error => console.error(error));
-    });
-    app.put('/quotes', (request, response) => {
-        quotesCollection.findOneAndUpdate(
-            { name: '' }, 
-        { 
-            $set: {
-                name: request.body.name,
-                quote: request.body.quote
-            }
-        }, 
-        {
-            upsert: true
-        })
-        .then(result => {
-            // console.log(result)
-            response.json('Changes saved')
-        })
-        .catch(error => console.error(error));
-    });
-    app.delete('/quotes', (request, response) => {
-        quotesCollection.deleteOne(
-            { name: request.body.name }
-        )
-        .then(result => {
-            if (result.deletedCount === 0) {
-                return response.json('No changes to remove');
-            }
-            response.json('Changes removed');
-        })
-    })
+    console.log(`Connected to ${DB_NAME} Database`);
+    db = client.db(DB_NAME);
 })
 .catch(error => console.error(error));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+app.get('/', (request, response) => {
+    db.collection(COLL_NAME).find().sort({likes: -1}).toArray()
+    .then(data => {
+        response.render('index.ejs', { quotes: data });
+    })
+    .catch(error => console.error(error));
+});
+
+app.post('/addQuotes', (request, response) => {
+    db.collection(COLL_NAME).insertOne({name: request.body.name, quote: request.body.quote, likes: 0})
+    .then(data => {
+        console.log('Quote & Author added.');
+        response.redirect('/');
+    })
+    .catch(error => console.error(error));
+});
+
+app.put('/addUpvote', (request, response) => {
+    db.collection(COLL_NAME).findOneAndUpdate({name: request.body.name, quote: request.body.quote, likes: request.body.likes}, {
+        $set: {
+            likes: request.body.likes + 1
+          }
+    },{
+        sort: {_id: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+})
+
+//     app.put('/quotes', (request, response) => {
+//         quotesCollection.findOneAndUpdate({ name: '' }, { $set: {name: request.body.name, quote: request.body.quote} }, { upsert: true })
+//         .then(result => {
+//             response.json('Changes saved')
+//         })
+//         .catch(error => console.error(error));
+// });
+
+app.delete('/deleteQuote', (request, response) => {
+    db.collection(COLL_NAME).deleteOne({name: request.body.name})
+    .then(result => {
+        console.log('Changed removed')
+        response.json('Changes removed');
+    })
+});
 
 app.listen(PORT, () => {
     console.log(`Listening to ${PORT}`);
